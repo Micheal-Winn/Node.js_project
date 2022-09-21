eeb39e0e817ac488d6a05fe8427da0f30d11f5b8
Micheal-Winn <thant.zin.winnnn@gmail.com>
2022-09-21 10:57:27 +0630

    fix

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var AuthRouter = require('./routes/userInfo');
-const auth = require('./middleware/authentication')
+
 
 var app = express();
 
--- routes/userInfo.js
+++ routes/userInfo.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var user = require('../controller/UserController')
+// const auth = require('./middleware/authentication')
 
 
 router.post('/regime',user.registerUser);
